e1e59c99fb67241dc36bdf99cf173e4b2431dbe5
kusabashira <kusabashira227@gmail.com>
2016-01-28 14:48:25 +0900

    Refactor order of replace in escape

--- js/anond-escape.js
+++ js/anond-escape.js
@@ -2,18 +2,18 @@
   function escape(text) {
     return text
       .replace(/&/g, "&#x26;")
+      .replace(/</g, "&#x3c;")
       .replace(/>/g, "&#x3e;")
+      .replace(/&#x3c;!--/g, "<!--")
       .replace(/--&#x3e;/g, "-->")
+      .replace(/&#x3c;&#x3c;/g, "<<")
       .replace(/&#x3e;&#x3e;/g, ">>")
       .replace(/&#x3e;\|/g, ">|")
-      .replace(/</g, "&#x3c;")
-      .replace(/&#x3c;!--/g, "<!--")
-      .replace(/&#x3c;&#x3c;/g, "<<")
       .replace(/\|&#x3c;/g, "|<")
       .replace(/>\|([\d\D]*?)\|</g, function(_, pre) {
         var escaped = pre
-          .replace(/>>/g, "&#x3e;&#x3e;")
           .replace(/<</g, "&#x3c;&#x3c;")
+          .replace(/>>/g, "&#x3e;&#x3e;")
           ;
         return ['>|', escaped, '|<'].join('');
       })
